d720fb24635f6027f78926e06d25c8051ab39f7c
rellikoder <146459546+rellikoder@users.noreply.github.com>
2025-10-25 21:21:14 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-# Переменные
 disk="/dev/sda"
 
-
-# Диск
 wipefs --all $disk
-
 parted -s $disk mklabel gpt
-parted -s $disk mkpart "EFI System" fat32 1MiB 513MiB
+parted -s $disk mkpart 'EFI System' fat32 1MiB 513MiB
 parted -s $disk set 1 esp on
-parted -s $disk mkpart "Linux swap" linux-swap 513Mib 16897MiB 
-parted -s $disk mkpart "Linux filesystem" ext4 16897Mib 100%
+parted -s $disk mkpart 'Linux swap' linux-swap 513MiB 16897MiB 
+parted -s $disk mkpart 'Linux filesystem' ext4 16897MiB 100%
 
 mkfs.fat -F32 "${disk}1"
 mkswap "${disk}2"
 swapon "${disk}2"
-mkfs.ext4 "${disk}3"
+mkfs.ext4 -F "${disk}3"
